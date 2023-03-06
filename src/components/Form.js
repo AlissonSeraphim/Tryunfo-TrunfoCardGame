bc750f48f8ddba14837541b7ed383cd3f173b355
AlissonSeraphim <projetoseraphim@gmail.com>
2023-03-06 17:02:19 -0300

    Requisito 1 OK

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,7 +4,43 @@ class Form extends React.Component {
   render() {
     return (
       <div>
-        <h1>Tryunfo</h1>
+        <label>
+          Nome:
+          <input type="text" data-testid="name-input" />
+        </label>
+        <label>
+          Descrição:
+          <textarea data-testid="description-input" />
+        </label>
+        <label>
+          Primeiro Atributo:
+          <input type="number" data-testid="attr1-input" />
+        </label>
+        <label>
+          Segundo Atributo:
+          <input type="number" data-testid="attr2-input" />
+        </label>
+        <label>
+          Terceiro Atributo:
+          <input type="number" data-testid="attr3-input" />
+        </label>
+        <label>
+          Imagem:
+          <input type="text" data-testid="image-input" />
+        </label>
+        <label>
+          Raridade:
+          <select data-testid="rare-input">
+            <option value="normal">Normal</option>
+            <option value="raro">Raro</option>
+            <option value="muito raro">Muito Raro</option>
+          </select>
+        </label>
+        <label>
+          Super Trunfo ?:
+          <input type="checkbox" data-testid="trunfo-input" />
+        </label>
+        <button type="submit" data-testid="save-button">Salvar</button>
       </div>
     );
   }
